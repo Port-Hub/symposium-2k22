132d0880b622d3abf9fdf6a57c38ac39ff3deb18
RahulRST <srahuljayaraj@gmail.com>
2022-10-01 17:18:56 +0530

    home changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Outlet } from "react-router-dom";
 import Events from "./pages/events";
-import ParaX from "./components/parax";
+import Home from "./pages/home";
 import Layout from "./components/layout";
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route exact path="/" element={<ParaX />} />
+        <Route exact path="/" element={<Home />} />
         <Route element={<Layout> <Outlet /> </Layout>} >
           <Route path="/events" element={<Events />}></Route>
         </Route>
