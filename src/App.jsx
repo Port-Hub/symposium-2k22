b0ae6d8da2699bde302dd94bea138a0ee94bf6b5
RahulRST <srahuljayaraj@gmail.com>
2022-10-01 15:05:51 +0530

    daisyui

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Events from "./pages/events";
 import ParaX from "./components/parax";
 
 const App = () => {
   return (
-    <>
-    <ParaX/>
-    </>
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<ParaX />} />
+        <Route exact path="/events" element={<Events />} />
+      </Routes>
+    </Router>
   );
 };
 
