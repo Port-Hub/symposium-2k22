9ff4bf1819ccd3184f8928c0cd8f53e9ba00d527
RahulRST <srahuljayaraj@gmail.com>
2022-10-03 22:30:02 +0530

    hash links :)

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { HashLink as Link } from "react-router-hash-link";
 
 import Favicon from "../assets/icons/favicon.png";
 import Golf from "../assets/events/code-golfing.png";
