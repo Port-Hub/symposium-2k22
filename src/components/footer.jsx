e507cd8bfd09eb856c7d58bc6a00ea5ff7553679
RahulRST <srahuljayaraj@gmail.com>
2022-10-04 21:45:01 +0530

    contrib :)

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -7,26 +7,25 @@ import { faR, faL, faA } from "@fortawesome/free-solid-svg-icons"
 const Footer = () => {
     return(
         <footer className="flex flex-row flex-wrap footer justify-between p-5 font-poppins">
-            <div className="grid-flow-col items-center">
+            <div className="grid-flow-col self-center items-center">
                 <img className="h-5" src={Favicon} />
                 <p className="font-poppins text-xs"> © 2022 LICET - All right reserved</p>
             </div>
-            <div className="gap-4 grid-flow-col">
-                <p>Shoutouts to </p>
+            <div className="gap-4 grid-flow-col self-center">
                 <a href ="https://github.com/RahulRST">
-                <FontAwesomeIcon icon={faR} /> 
+                <FontAwesomeIcon icon={faR} className="h-2 border rounded-full p-1 border-black" /> 
             </a>
             <a href ="https://github.com/RomarioKavin1">
-                <FontAwesomeIcon icon={faR} /> 
+                <FontAwesomeIcon icon={faR} className="h-2 border rounded-full p-1 border-black" /> 
             </a>
             <a href ="https://github.com/LeoFranklin015">
-                <FontAwesomeIcon icon={faL} /> 
+                <FontAwesomeIcon icon={faL} className="h-2 border rounded-full p-1 border-black" /> 
             </a>
             <a href ="https://github.com/Adelin-Brianna">
-                <FontAwesomeIcon icon={faA} /> 
+                <FontAwesomeIcon icon={faA} className="h-2 border rounded-full p-1 border-black" /> 
             </a>
             </div> 
-            <div className="gap-4 grid-flow-col">
+            <div className="gap-4 grid-flow-col self-center">
                 <a href="https://www.instagram.com/cse_eicon/" target="_blank">
                     <FontAwesomeIcon icon={faInstagram} />
                 </a> 
